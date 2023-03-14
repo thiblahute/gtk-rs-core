a70aa21651cac1e79cc9fd81fea6dc3c670bc704
Thibault Saunier <tsaunier@igalia.com>
2023-03-14 15:40:33 -0300

    Regenerate with latest gir
    Remove the link attribute from the sys crates as it's not needed anymore.

--- glib/sys/src/lib.rs
+++ glib/sys/src/lib.rs
@@ -2409,8 +2409,6 @@ impl ::std::fmt::Debug for GVariantType {
     }
 }
 
-#[link(name = "gobject-2.0")]
-#[link(name = "glib-2.0")]
 extern "C" {
 
     //=========================================================================
